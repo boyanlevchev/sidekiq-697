e0444330c9fa32ed9fd8c6d56f54810ffaf3301a
Le Wagon <boyanlevchev@gmail.com>
2021-09-01 10:18:37 +0100

    Sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Sidekiq Web UI, only for admins.
+
   require "sidekiq/web"
+  # Specify a route to access sidekiq gui dashboard
+  # Only accessible if user is an admin
   authenticate :user, ->(user) { user.admin? } do
     mount Sidekiq::Web => '/sidekiq'
   end
